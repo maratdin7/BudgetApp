c7f5ef4f2f0f4c35fd78f87fc993733dc5ecd166
Динмухаметов Марат Линарович <maratdin7@gmail.com>
2021-10-13 04:53:53 +0300

    Recycler view for user entity.

--- app/src/main/java/com/example/budget/client/api/UserApi.kt
+++ app/src/main/java/com/example/budget/client/api/UserApi.kt
@@ -2,10 +2,12 @@ package com.example.budget.client.api
 
 import com.example.budget.dto.UserEntity
 import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface UserApi {
+    @FormUrlEncoded
     @POST("allUsersInGroup")
-    suspend fun getAllUsersInGroup(@Query("groupId") groupId: Int): Response<List<UserEntity>>
+    suspend fun getAllUsersInGroup(@Field("groupId") groupId: Int): Response<List<UserEntity>>
 }
